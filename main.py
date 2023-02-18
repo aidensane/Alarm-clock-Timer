ace63592eee11860fe1e40246a6aecc61ad392cb
aidensane <98586059+aidensane@users.noreply.github.com>
2023-02-18 15:45:08 +0100

    get input from user

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from playsound import playsound
 import time
 
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
 
 
 def alarm(seconds):
     time_elapsed = 0
 
+    print(CLEAR)
     while time_elapsed < seconds:
         time.sleep(1)
         time_elapsed += 1
@@ -14,7 +17,13 @@ def alarm(seconds):
         minutes_left = time_left // 60
         seconds_left = time_left % 60
 
-        print(f"{minutes_left:02d}:{seconds_left:02d}")
+        print(
+            f"{CLEAR_AND_RETURN}Alarm will sound in: {minutes_left:02d}:{seconds_left:02d}")
+
+    playsound('alarm.mp3')
 
 
-alarm(10)
+minutes = int(input("How many minutes to wait: "))
+seconds = int(input("How many seconds to wait: "))
+total_seconds = minutes + seconds
+alarm(total_seconds)
